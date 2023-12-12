7a00d3ee39b8297df9fd5dcccf4be0a721a587f9
Tim Goyer <tim.goyer@gmail.com>
2023-12-11 20:36:37 -0600

    Hero cleanup

--- src/components/PrimaryFeatures.jsx
+++ src/components/PrimaryFeatures.jsx
@@ -27,128 +27,51 @@ const features = [
 ]
 
 export function PrimaryFeatures() {
-  let [tabOrientation, setTabOrientation] = useState('horizontal')
-
-  useEffect(() => {
-    let lgMediaQuery = window.matchMedia('(min-width: 1024px)')
-
-    function onMediaQueryChange({ matches }) {
-      setTabOrientation(matches ? 'vertical' : 'horizontal')
-    }
-
-    onMediaQueryChange(lgMediaQuery)
-    lgMediaQuery.addEventListener('change', onMediaQueryChange)
-
-    return () => {
-      lgMediaQuery.removeEventListener('change', onMediaQueryChange)
-    }
-  }, [])
+  const [tabOrientation, setTabOrientation] = useState('horizontal')
+  const whyUsUrl = backgroundImage.src;
 
   return (
     <section
       id="why-us"
       aria-label="why-us"
-      className="bg-white-600 relative overflow-hidden pb-28 pt-20 sm:py-32"
+      style={{'--image-url': `url(${whyUsUrl})`}} 
+      className="flex flex-col items-start overflow-hidden bg-white-600 py-20 px-8 sm:px-12 md:px-16 bg-cover bg-no-repeat bg-top-left bg-[image:var(--image-url)]"
     >
-      <Image
-        className="absolute left-1/4 top-1/3 max-w-none translate-x-[-44%] translate-y-[-32%]"
-        src={backgroundImage}
-        alt=""
-        width={2245}
-        height={1636}
-        unoptimized
-        style={{ filter: 'brightness(0.8)' }}
-      />
-      <Container className="relative grid-cols-12">
-        <div className="grid-cols-6"></div>
-        <div className="mr-10 grid-cols-12">
-          <div className="grid grid-cols-12">
-            <h2 className="col-span-6 col-start-7 font-display text-2xl text-white sm:text-4xl md:text-5xl">
-              Unlock Your Potential with Our Prestigious Firm
-            </h2>
-            <p className="text-1xl col-span-6 col-start-7 mt-10 tracking-tight text-white sm:text-3xl md:text-2xl">
-              At our firm, we offer a unique opportunity for talented
-              individuals to join our prestigious financial planning practice.
-              With our regal design and industry expertise, we provide the
-              perfect environment for you to thrive and make a difference in the
-              lives of our clients.
+      <Container className="flex flex-col gap-4 bg-slate-500/75 lg:bg-slate-500/50 mx-0 lg:w-[40%] md:w-[100%] p-10 rounded-4xl">
+        <div className="flex flex-col gap-4">
+          <h2 className="text-lg font-bold text-white lg:text-3xl font-display sm:text-1xl md:text-2xl">
+            Unlock Your Potential with Our Prestigious Firm
+          </h2>
+          <p className="text-sm text-white lg:text-1xl font-display sm:text-md md:text-lg">
+            At our firm, we offer a unique opportunity for talented
+            individuals to join our prestigious financial planning practice.
+            With our regal design and industry expertise, we provide the
+            perfect environment for you to thrive and make a difference in the
+            lives of our clients.
+          </p>
+        </div>
+        <div className="flex flex-col gap-4 xl:flex-row">
+        {features.map((feature) => (
+          <div key={feature.title} className="w-full pl-8 xl:w-1/2">
+            <h3 className="text-lg font-bold text-white font-display sm:text-md md:text-lg lg:text-1xl">
+              {feature.title}
+            </h3>
+            <p className="text-sm text-white lg:text-1xl font-display sm:text-md md:text-lg">
+              {feature.description}
             </p>
           </div>
-          <div className="mt-2 grid grid-cols-12 lg:mt-20">
-            <Tab.Group
-              as="div"
-              className="col-span-5 col-start-7 mr-4 grid justify-end sm:mx-0 sm:overflow-visible"
-              vertical={tabOrientation === 'horizontal'}
-              grid-column="7/13"
-            >
-              {({ selectedIndex }) => (
-                <>
-                  <div className="justify-ends -mx-4 flex pb-4 sm:mx-0 sm:overflow-visible sm:pb-0 lg:col-span-5">
-                    <Tab.List className="relative z-10 flex gap-x-4 whitespace-nowrap px-4 sm:mx-auto sm:px-0 lg:mx-0 lg:block lg:gap-x-0 lg:gap-y-1 lg:whitespace-normal">
-                      {features.map((feature, featureIndex) => (
-                        <div
-                          key={feature.title}
-                          className={clsx(
-                            'group relative rounded-full px-4 py-1 lg:rounded-l-xl lg:rounded-r-none lg:p-6	'
-                          )}
-                        >
-                          <h3>
-                            <Tab
-                              className={clsx(
-                                'cursor-default font-display text-xl text-white [&:not(:focus-visible)]:focus:outline-none'
-                              )}
-                            >
-                              <span className="absolute inset-0 rounded-full text-white lg:rounded-l-xl lg:rounded-r-none" />
-                              {feature.title}
-                            </Tab>
-                          </h3>
-                          <p
-                            className={clsx(
-                              'mt-2 hidden text-lg text-white lg:block'
-                            )}
-                          >
-                            {feature.description}
-                          </p>
-                        </div>
-                      ))}
-                    </Tab.List>
-                  </div>
-                </>
-              )}
-            </Tab.Group>
-          </div>
-          <div className="mr-40 mt-6 flex justify-end">
-            <Button
-              grid-column="10/11"
-              style={{
-                color: 'black',
-                backgroundColor: '#32A665',
-                borderRadius: '0.375rem',
-                border: '1px solid black',
-                padding: '0.5rem 1rem',
-                fontSize: '1.125rem',
-                cursor: 'pointer',
-                marginRight: '30px',
-              }}
-            >
-              <span>Learn More</span>
-            </Button>
-            <Button
-              grid-column="12/13"
-              style={{
-                borderRadius: '1.5rem',
-                backgroundColor: 'white',
-                padding: '0.5rem 1rem',
-                fontSize: '1.125rem',
-                color: 'black',
-                cursor: 'pointer',
-                border: '1.5px solid black',
-              }}
-              href="/register"
-            >
-              Get Started
-            </Button>
-          </div>
+        ))}
+        </div>
+        <div className="grid grid-rows-2 gap-2 md:grid-cols-2 md:grid-rows-[unset]">
+          <Button className="text-black bg-[#32A665] rounded-3xl border border-black hover:border-[#32A665] py-2 px-4 text-lg cursor-pointer">
+            <span>Learn More</span>
+          </Button>
+          <Button 
+            className="py-2 px-4 text-lg !text-black hover:!text-white bg-white border border-black hover:border-white cursor-pointer rounded-3xl"
+            href="/register"
+          >
+            Get Started
+          </Button>
         </div>
       </Container>
     </section>
